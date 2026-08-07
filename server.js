01739c2e6817c49ee1b60a99317ee701323fe809
shwetabotx <s.j.jadeja69@gmail.com>
2026-08-07 14:39:26 +0530

    project set-up

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+
+    res.send("MERN Server is running");
+
+});
+
+
+app.listen(PORT, () => {
+
+    console.log(`Server running on http://localhost:${PORT}`);
+
+});
